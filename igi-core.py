eba0a9fd1278f1694e62f1b54308f580a91dbb25
REMOL <remikp207@gmail.com>
2022-02-21 22:47:52 +0100

    Create igi-core.py

---
+++ igi-core.py
@@ -0,0 +1,22 @@
+# The most basic and important vars and funcs to get started :)
+
+### VARS ###
+
+currFileLine = 0
+rbhft = "lol"
+
+### VARS ###
+#-#
+### FUNCS ###
+
+def readFile(fileName="main.igi"):
+    igiEntryFile = open(fileName, "r", "utf-8")
+
+    print(igiEntryFile)
+
+    igiEntryFile.close()
+
+def somethingMoreHere():
+    pass
+
+### FUNCS ###
